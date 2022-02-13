3c741a304611ed5a2e956193818efc21d85741da
daehwan2 <dhdh0482@naver.com>
2022-02-13 22:18:19 +0900

    Feat : Redux 활용한 로그인 페이지 구현

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
 
-function LoginPage() {
-  return <div>LoginPage</div>;
+function LoginPage(props) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onEmailHnadler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onPasswordHnadler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    let body = {
+      email,
+      password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        navigate("/");
+      } else {
+        alert("error");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={email} onChange={onEmailHnadler} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={onPasswordHnadler} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
